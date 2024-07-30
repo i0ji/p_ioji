fe0f4db4b834cde194837df8abcc6327d31316f7
i0ji <ioji@inbox.ru>
2024-07-30 17:16:43 +0300

    feature: starting main markup;add alias, sass, prettier

---
+++ src/components/HammerButton/hammerButton.tsx
@@ -0,0 +1,8 @@
+import styles from "./hammerButton.module.scss"
+
+export default function HammerButton() {
+  return (
+    <button className={styles.hammerButton}>
+    </button>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  build: {
+    assetsInlineLimit: 0,
+    outDir: "dist",
+    minify: true,
+  },
+  resolve: {
+    alias: {
+      components: "/src/components",
+      declarations: "src/declarations",
+      assets: "/src/assets",
+      data: "/src/data",
+      styles: "/src/styles",
+      src: ".",
+    },
+  },
+  server: {
+    hmr: true,
+  },
+});
