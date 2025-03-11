ed639f464bc8a82ccb4fbe45a4628541807ca0cd
Ivan Vasilevich <ioji@inbox.ru>
2025-03-11 11:54:21 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    //FIXME
+    unoptimized: false
+  },
 };
 
 export default nextConfig;
