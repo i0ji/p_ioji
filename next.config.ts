0944926b1bf563dae6ba24678e385f3865cd26ec
Ivan Vasilevich <ioji@inbox.ru>
2025-03-13 08:22:05 +0300

    chore/deploy: test#11

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
@@ -7,12 +9,14 @@ const nextConfig: NextConfig = {
     //FIXME
     unoptimized: false,
   },
-  basePath: '',
-  assetPrefix: '/',
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  basePath: isProd ? '/p_ioji' : '',
+  assetPrefix: isProd ? '/p_ioji/' : '',
   webpack: (config) => {
-    config.resolve.alias['@/styles'] = './src/styles/*';
+    config.resolve.alias['@styles'] = './src/styles/*';
     return config;
   },
 };
 
 export default nextConfig;
+ 
