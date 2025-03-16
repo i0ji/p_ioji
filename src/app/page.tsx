06382ccfe9335bc3c0a07ae634ffc2e3ff36a611
i0ji <ioji@inbox.ru>
2025-03-16 11:26:04 +0300

    chore/update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,20 @@ import Link from 'next/link';
 import s from './page.module.scss';
 
 export default function Home() {
-  //CONSOLE
-  console.log('v: 0.0.2');
-<<<<<<< HEAD
-=======
+    //CONSOLE
+    console.log('v: 0.0.2');
+    //LATER
+    const tag = 'ioji';
 
-  //LATER
-  const tag = 'ioji';
->>>>>>> 4ed75f5c60d36b8eaccf949221890f6629b62b28
-
-  return (
-    <main className={s.entrance}>
-      <div className={s.entrance__window}>
-        <Link href="/tabs">
-          <h1 className={`fontBold ${s.glitch}`} data-text={tag}>
-            {tag}
-          </h1>
-        </Link>
-      </div>
-    </main>
-  );
+    return (
+        <main className={s.entrance}>
+            <div className={s.entrance__window}>
+                <Link href="/tabs">
+                    <h1 className={`fontBold ${s.glitch}`} data-text={tag}>
+                        {tag}
+                    </h1>
+                </Link>
+            </div>
+        </main>
+    );
 }
