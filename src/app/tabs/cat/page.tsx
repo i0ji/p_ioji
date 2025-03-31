129e3fa1d5247b59a91dc21e262b465b1571839c
Ivan Vasilevich <ioji@inbox.ru>
2025-03-31 08:07:48 +0300

    chore/update deps

--- src/app/tabs/cat/page.tsx
+++ src/app/tabs/cat/page.tsx
@@ -1,11 +1,10 @@
-import {CommonButton} from 'components/index'
+import { CommonButton } from 'components/index';
 
 export default function CatPage() {
-
-    return (
-        <div>
-            <CommonButton text={'HOME'} type={'home'}/>
-            something interesting about my lovely cat!
-        </div>
-    );
+  return (
+    <div>
+      <CommonButton text={'HOME'} type={'home'} />
+      something interesting about my lovely cat!
+    </div>
+  );
 }
