09432073618ee4d16a81ccddb18efbc1e1e6c9b0
Ivan Vasilevich <ioji@inbox.ru>
2025-07-29 08:05:43 +0300

    build(components): staged commits

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,13 +7,14 @@ export default function Hero() {
   return (
     <section
       id="hero"
-      className="h-screen w-full snap-start border-red-700 border-l-purple-950 bg-gradient-to-br from-cyan-800 from-5%"
+      className="flex h-screen w-full snap-start items-center border-red-700 border-l-purple-950 bg-gradient-to-br from-cyan-800 from-5%"
     >
-      <div className="mx-auto flex h-full max-w-screen-xl items-center justify-center">
+      <div
+        style={{ border: "1px solid red" }}
+        className="mx-auto flex max-w-screen-2xl items-center justify-center"
+      >
         {heroData.map((heroData: heroModel) => (
-          <Card 
-          key={nanoid()}
-          data={heroData} />
+          <Card key={nanoid()} data={heroData} />
         ))}
       </div>
     </section>
