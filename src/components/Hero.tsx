d5d54c016f5e575687c95c8137f213c1d95deb0c
i0ji <ioji@inbox.ru>
2025-07-30 19:35:47 +0300

    feat(components): rework hero cards

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,40 +2,17 @@ import heroData from "data/data";
 
 import { Card } from "./ui";
 import { nanoid } from "nanoid";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination } from "swiper/modules";
-
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-
-import SwiperCore from "swiper/core";
 
 export default function Hero() {
-  SwiperCore.use([Pagination]);
-
   return (
     <section
       id="hero"
       className="flex h-screen w-full snap-start items-center justify-center border-l-purple-950 bg-gradient-to-br from-cyan-800 from-5%"
     >
-      <div className="mx-auto flex h-[90%] w-full flex-wrap items-center justify-around">
-        <Swiper
-          modules={[Navigation]}
-          slidesPerView={2}
-          navigation
-          pagination={{ type: "progressbar" }}
-          className="relative h-[70] w-full max-w-[80vw] pt-10"
-        >
-          {heroData.map((item: heroModel) => (
-            <SwiperSlide
-              key={nanoid()}
-              className="h-ull"
-            >
-              <Card data={item} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+      <div className="mx-auto flex  h-[90%] w-full flex-wrap items-center justify-between">
+        {heroData.map((item: heroModel) => (
+          <Card key={nanoid()} data={item} />
+        ))}
       </div>
     </section>
   );
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -3,13 +3,13 @@ import { nanoid } from "nanoid";
 export default function Card({ data }: { data: heroModel }) {
   return (
     <div
-      className="mx-auto my-8 flex h-[70vh] w-[30vw] transform flex-col rounded-lg bg-gray-800 p-4 text-stone-400 shadow-lg drop-shadow-lg transition-transform duration-200 ease-linear hover:scale-[1.01]"
+      className="mx-auto my-8 flex h-[30vh] w-[25vw] transform flex-col justify-between rounded-lg bg-gray-800 p-4 text-stone-400 shadow-lg drop-shadow-lg transition-transform duration-200 ease-linear hover:scale-[1.01]"
       data-carousel-item
     >
-      <h6 className="mb-2 flex h-[15%] bg-green-500 text-xl font-bold">
+      <h6 className="mb-2 flex h-auto text-xl font-bold">
         {data.title}
       </h6>
-      <div className="flex h-auto flex-auto flex-col justify-between bg-yellow-300">
+      {/* <div className="flex h-auto flex-auto flex-col  overflow-y-scroll justify-between bg-yellow-100">
         <ul className="flex list-disc flex-col justify-between pl-5 text-justify">
           {data.responsibilities.map((unit) => (
             <li key={nanoid()}>
@@ -17,11 +17,11 @@ export default function Card({ data }: { data: heroModel }) {
             </li>
           ))}
         </ul>
-      </div>
-      <div className="flex h-[30%] flex-col justify-between bg-red-300">
+      </div> */}
+      <div className="flex h-auto flex-col justify-between">
         <hr className="h-[1px] w-full bg-stone-400 opacity-50" />
-        <p className="w-full">Technologies: </p>
-        <ul className="flex flex-initial flex-wrap">
+        <p className="w-full my-2">Technologies: </p>
+        <ul className="flex flex-initial flex-wrap ">
           {data.technologies.map((unit) => (
             <li key={nanoid()} className="pr-2">
               <p>{unit}</p>
@@ -29,7 +29,7 @@ export default function Card({ data }: { data: heroModel }) {
           ))}
         </ul>
 
-        <button className="group relative mb-2 me-2 inline-flex w-1/3 items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800">
+        <button className="group relative mt-2 me-2 inline-flex w-full items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800">
           <span className="relative w-full rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-transparent dark:bg-gray-900 group-hover:dark:bg-transparent">
             Read more
           </span>
