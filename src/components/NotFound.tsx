55e3c938408f00945ca2c7c9213e6fd8908d29bc
i0ji <adm.ivan.udodenko@RTARU.COM>
2025-08-14 13:49:04 +0300

    build(full product): deploy/15

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NotFound() {
   return (
     <div className="h-full w-full">
-      <h1>404 - Page Not Found</h1>
+      <h1>404 - Page Not Found!!!!</h1>
       <p>Извините, страница не найдена.</p>
       <a href="/">Вернуться на главную</a>
     </div>
