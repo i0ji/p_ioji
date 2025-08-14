61c894f6284fb3b9282bfcb32694285f2e12d732
i0ji <adm.ivan.udodenko@RTARU.COM>
2025-08-14 13:35:57 +0300

    build(full product): deploy/12

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { NotFound } from "components/index";
 import "./index.scss";
@@ -8,11 +8,11 @@ import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
-      <Routes location={location} key={location.pathname}>
+    <BrowserRouter>
+      <Routes>
         <Route path="/" element={<App />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
