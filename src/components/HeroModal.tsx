9460b42f6f7a9317addbbaae33b70b360b60fb76
i0ji <adm.ivan.udodenko@RTARU.COM>
2025-08-14 13:16:16 +0300

    build(full product): deploy/9

--- src/components/HeroModal.tsx
+++
@@ -1,9 +0,0 @@
-import { Modal } from "components/index";
-
-export default function HeroModal({ isOpen, handleClose, description }) {
-  return (
-    <Modal isOpen={isOpen} handleClose={handleClose}>
-      {description}
-    </Modal>
-  );
-}
