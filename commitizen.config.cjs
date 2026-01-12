6663df86f733c330239b5c7f509d8dd573f35886
i0ji <ioji@inbox.ru>
2026-01-12 15:02:46 +0300

    build: refactor: 0.3.0

---
+++ commitizen.config.cjs
@@ -0,0 +1,63 @@
+"use strict";
+
+module.exports = {
+  types: [
+    {
+      value: "build",
+      name: "build:     out builds",
+    },
+    {
+      value: "chore",
+    },
+    {
+      value: "ci",
+      name: "ci/cd",
+    },
+    {
+      value: "docs",
+      name: "docs:      docs update",
+    },
+    {
+      value: "feat",
+      name: "feat:      add new features",
+    },
+    { value: "fix", name: "fix:       fix bugs" },
+    {
+      value: "refactor",
+    },
+    {
+      value: "revert",
+      name: "revert:    reset/revert commit",
+    },
+    {
+      value: "style",
+      name: "style:     style/implementation features",
+    },
+    { value: "tests" },
+  ],
+
+  scopes: [
+    { name: "components" },
+    { name: "environment" },
+    { name: "full product" },
+  ],
+
+  messages: {
+    type: "Changes type:",
+    scope: "\nChoose scope:",
+    customScope: "Custom scope:",
+    subject: "Short description:\n",
+    body: 'Full description. Use "|" for new row:\n',
+    breaking: "Breaking changes:\n",
+    footer: "Meta (version, issues):\n",
+    confirmCommit: "Confirm",
+  },
+
+  allowCustomScopes: true,
+
+  allowBreakingChanges: false,
+
+  footerPrefix: "Version:",
+
+  subjectLimit: 72,
+};
